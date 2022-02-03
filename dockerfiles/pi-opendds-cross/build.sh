dab8563f8925fa96b3a978720994d463ebd88501
Chip Jones <jonesc@objectcomputing.com>
2022-02-03 16:24:38 -0600

    Update pi-opendds dockerfile and build script.

--- dockerfiles/pi-opendds-cross/build.sh
+++ dockerfiles/pi-opendds-cross/build.sh
@@ -5,28 +5,30 @@ OPENDDS_GIT_REPO=https://github.com/objectcomputing/OpenDDS
 OPENDDS_MAKE_JOBS=4
 OPENDDS_PREFIX=${BUILD_DIR}/pi-opendds
 
-wget -nc http://download.objectcomputing.com/OpenDDS/resources/gcc-8.2.0-rpi.tar.bz2
-tar xjf gcc-8.2.0-rpi.tar.bz2
+wget -nc -O gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz \
+    "https://developer.arm.com/-/media/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz?revision=d0b90559-3960-4e4b-9297-7ddbc3e52783&hash=6F50B04F08298881CA3596CE99E5ABB3925DEB24"
 
-ln -sf $BUILD_DIR/cross-pi-gcc-8.2.0 /opt/cross-pi-gcc
+tar -xvf gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf.tar.xz
 
-PATH+=:/opt/cross-pi-gcc/bin
+CROSS_PI_GCC=${BUILD_DIR}/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 
-wget -nc https://www.openssl.org/source/openssl-1.1.1a.tar.gz
-tar xzf openssl-1.1.1a.tar.gz
+PATH+=:${CROSS_PI_GCC}/bin
+
+wget -nc https://www.openssl.org/source/openssl-1.1.1m.tar.gz
+tar xzf openssl-1.1.1m.tar.gz
 
 export BUILD_ROOT=$BUILD_DIR
 
-(cd openssl-1.1.1a && \
-	  ./Configure --cross-compile-prefix=arm-linux-gnueabihf- linux-armv4 && \
+(cd openssl-1.1.1m && \
+	  ./Configure --cross-compile-prefix=arm-none-linux-gnueabihf- linux-armv4 && \
 	  make && make install DESTDIR=${BUILD_ROOT}/pi-openssl)
 
 printf "\
 	set(CMAKE_SYSTEM_NAME Linux)\n \
 	set(CMAKE_SYSTEM_PROCESSOR arm)\n \
-	set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)\n \
-	set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)\n \
-	set(CMAKE_FIND_ROOT_PATH /opt/cross-pi-gcc/arm-linux-gnueabihf)\n \
+	set(CMAKE_C_COMPILER arm-none-linux-gnueabihf-gcc)\n \
+	set(CMAKE_CXX_COMPILER arm-none-linux-gnueabihf-g++)\n \
+	set(CMAKE_FIND_ROOT_PATH /opt/cross-pi-gcc/arm-none-linux-gnueabihf)\n \
 	set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)\n \
 	set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)\n \
 	set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)\n \
@@ -34,10 +36,10 @@ printf "\
 	set(THREADS_PTHREAD_ARG 2)\n \
 	" > ${BUILD_ROOT}/PiToolchain.cmake
 
-wget -nc http://apache.cs.utah.edu/xerces/c/3/sources/xerces-c-3.2.2.tar.gz
-tar xzf xerces-c-3.2.2.tar.gz
+wget -nc https://dlcdn.apache.org//xerces/c/3/sources/xerces-c-3.2.3.tar.gz
+tar xzf xerces-c-3.2.3.tar.gz
 
-(cd xerces-c-3.2.2 && \
+(cd xerces-c-3.2.3 && \
   mkdir -p build-pi && cd build-pi && \
   cmake -DCMAKE_TOOLCHAIN_FILE=${BUILD_ROOT}/PiToolchain.cmake -DCMAKE_INSTALL_PREFIX=${BUILD_ROOT}/pi-xerces .. && \
   make && make install)
@@ -46,14 +48,18 @@ git clone --depth=1 -b ${OPENDDS_BRANCH} ${OPENDDS_GIT_REPO} pi-opendds
 
 (cd pi-opendds && \
   ./configure --prefix=$OPENDDS_PREFIX --ace-github-latest --security --no-tests --target=linux-cross \
-    --target-compiler=arm-linux-gnueabihf-g++ --openssl=$BUILD_ROOT/pi-openssl/usr/local --xerces3=$BUILD_ROOT/pi-xerces && \
+    --target-compiler=arm-none-linux-gnueabihf-g++ --openssl=$BUILD_ROOT/pi-openssl/usr/local --xerces3=$BUILD_ROOT/pi-xerces && \
   make -j${OPENDDS_MAKE_JOBS})
 
 tar czf pi-openssl.tar.gz pi-openssl
 tar czf pi-xerces.tar.gz pi-xerces
 tar czhf pi-opendds.tar.gz pi-opendds/build/target/ACE_TAO/ACE/lib pi-opendds/build/target/lib
 
-git clone git://git.drogon.net/wiringPi
-(cd wiringPi/wiringPi && make CC=arm-linux-gnueabihf-gcc)
+wget -O pigpio-v79.tar.gz https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
+tar xzvf pigpio-v79.tar.gz
 
-(cd wiringPi/wiringPi && mv libwiringPi.so.2.50 libwiringPi.so)
+(cd pigpio-79 && \
+    make CROSS_PREFIX=arm-none-linux-gnueabihf- && \
+    make install DESTDIR=${BUILD_ROOT}/pigpio prefix=)
+
+tar czvf pigpio.tar.gz pigpio
