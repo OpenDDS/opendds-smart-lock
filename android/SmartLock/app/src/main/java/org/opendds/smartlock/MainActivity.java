b7c63dc996ec1e1378cbbf63e641f405da1c5704
Chip Jones <jonesc@objectcomputing.com>
2022-02-07 15:23:38 -0600

    Remove unused native-lib code from Android app.

--- android/SmartLock/app/src/main/java/org/opendds/smartlock/MainActivity.java
+++ android/SmartLock/app/src/main/java/org/opendds/smartlock/MainActivity.java
@@ -229,10 +229,4 @@ public class MainActivity extends AppCompatActivity {
             addLock(status);
         }
     }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
