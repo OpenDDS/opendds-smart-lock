a8b88b843d919f03cafe832144ee3e42465151c8
Chad Elliott <elliottc@objectcomputing.com>
2023-03-27 11:02:43 -0500

    Added messages to help debug.

--- flutter/smartlock_idl_plugin/example/lib/screens/home.dart
+++ flutter/smartlock_idl_plugin/example/lib/screens/home.dart
@@ -18,6 +18,8 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+enum InitState { begin, downloading, downloaded, started }
+
 class _HomeState extends State<Home> {
   static const String _apiURL = "https://dpm.unityfoundation.io/api";
   static const String _username = "54";
@@ -26,6 +28,7 @@ class _HomeState extends State<Home> {
   final List<String> _text = [];
   final ScrollController _scrollController = ScrollController();
   smartlock_idl.Bridge? _bridge;
+  InitState _state = InitState.begin;
 
   // TESTING:
   smartlock_idl.LockState state = smartlock_idl.LockState.unlocked;
@@ -34,67 +37,73 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     _bridge = smartlock_idl.Bridge();
-    startBridge();
+    _startBridge();
   }
 
-  Future<Map<String, String>> downloadCerts(String directory) async {
+  Future<Map<String, String>> _downloadCerts(String directory) async {
+    setState(() => _state = InitState.downloading);
     Map<String, String> certs = {};
-    final dio = Dio();
-    dio.interceptors.add(CookieManager(CookieJar()));
+    try {
+      final dio = Dio();
+      dio.interceptors.add(CookieManager(CookieJar()));
 
-    var response = await dio.post("$_apiURL/login",
-        options: Options(
-          followRedirects: false,
-          headers: {"Content-Type": "application/json"},
-          validateStatus: (status) => status! < 500,
-        ),
-        data: {"username": _username, "password": _password});
-    if (response.statusCode == 303) {
-      Map<String, dynamic> headers = {"Content-Type": "text/plain"};
-      for (var entry in {
-        "id_ca": "applications/identity_ca.pem",
-        "perm_ca": "applications/permissions_ca.pem",
-        "perm_gov": "applications/governance.xml.p7s",
-        "perm_perms": "applications/permissions.xml.p7s?nonce=$_nonce",
-      }.entries) {
-        String filename = join(directory, basename(entry.value));
-        final int index = filename.indexOf('?');
-        if (index >= 0) {
-          filename = filename.substring(0, index);
+      var response = await dio.post("$_apiURL/login",
+          options: Options(
+            followRedirects: false,
+            headers: {"Content-Type": "application/json"},
+            validateStatus: (status) => status! < 500,
+          ),
+          data: {"username": _username, "password": _password});
+      if (response.statusCode == 303) {
+        Map<String, dynamic> headers = {"Content-Type": "text/plain"};
+        for (var entry in {
+          "id_ca": "applications/identity_ca.pem",
+          "perm_ca": "applications/permissions_ca.pem",
+          "perm_gov": "applications/governance.xml.p7s",
+          "perm_perms": "applications/permissions.xml.p7s?nonce=$_nonce",
+        }.entries) {
+          String filename = join(directory, basename(entry.value));
+          final int index = filename.indexOf('?');
+          if (index >= 0) {
+            filename = filename.substring(0, index);
+          }
+          response = await dio.download("$_apiURL/${entry.value}", filename,
+              options: Options(
+                headers: headers,
+                validateStatus: (status) => status! < 500,
+              ));
+          if (response.statusCode == 200) {
+            certs[entry.key] = filename;
+          }
         }
-        response = await dio.download("$_apiURL/${entry.value}", filename,
+        final filename = join(directory, "keypair.json");
+        response = await dio.download(
+            "$_apiURL/applications/key_pair?nonce=$_nonce", filename,
             options: Options(
               headers: headers,
               validateStatus: (status) => status! < 500,
             ));
         if (response.statusCode == 200) {
-          certs[entry.key] = filename;
+          final fp = File(filename);
+          Map keyPair = json.decode(fp.readAsStringSync());
+          fp.delete();
+
+          final cert = File(join(directory, "id_cert.pem"));
+          cert.writeAsStringSync(keyPair['public']);
+          final privateKey = File(join(directory, "id_private.pem"));
+          privateKey.writeAsStringSync(keyPair['private']);
+          certs['id_cert'] = cert.path;
+          certs['id_private'] = privateKey.path;
+          setState(() => _state = InitState.downloaded);
         }
       }
-      final filename = join(directory, "keypair.json");
-      response = await dio.download(
-          "$_apiURL/applications/key_pair?nonce=$_nonce", filename,
-          options: Options(
-            headers: headers,
-            validateStatus: (status) => status! < 500,
-          ));
-      if (response.statusCode == 200) {
-        final fp = File(filename);
-        Map keyPair = json.decode(fp.readAsStringSync());
-        fp.delete();
-
-        final cert = File(join(directory, "id_cert.pem"));
-        cert.writeAsStringSync(keyPair['public']);
-        final privateKey = File(join(directory, "id_private.pem"));
-        privateKey.writeAsStringSync(keyPair['private']);
-        certs['id_cert'] = cert.path;
-        certs['id_private'] = privateKey.path;
-      }
+    } catch (err) {
+      setState(() => _text.add(err.toString()));
     }
     return certs;
   }
 
-  void startBridge() async {
+  void _startBridge() async {
     // Read the ini config file from the assets
     final Directory documentsDirectory =
         await getApplicationDocumentsDirectory();
@@ -103,13 +112,13 @@ class _HomeState extends State<Home> {
     ini.writeAsStringSync(
         await rootBundle.loadString('assets/opendds_config.ini'));
 
-    final certs = await downloadCerts(path);
+    final certs = await _downloadCerts(path);
 
     // Start the bridge if we have all of the certs
     if (certs.containsKey('id_private')) {
       _bridge?.start(
-          snack,
-          tryToUpdateLock,
+          _snack,
+          _tryToUpdateLock,
           ini.path,
           certs['id_ca']!,
           certs['perm_ca']!,
@@ -117,10 +126,11 @@ class _HomeState extends State<Home> {
           certs['perm_perms']!,
           certs['id_cert']!,
           certs['id_private']!);
+      setState(() => _state = InitState.started);
     }
   }
 
-  void snack(String message) {
+  void _snack(String message) {
     // Check to see if this widget is mounted, since this is called
     // by the bridge from the native code and it would have no idea if
     // this screen is still around.
@@ -131,10 +141,23 @@ class _HomeState extends State<Home> {
     }
   }
 
-  void tryToUpdateLock(bool enabled, String id, smartlock_idl.LockState state) {
+  void _tryToUpdateLock(bool enabled, String id, smartlock_idl.LockState state) {
     setState(() {
       _text.add("$enabled - $id - $state");
     });
+  }
+
+  String _getStateMessage() {
+    switch(_state) {
+      case InitState.begin:
+        return "We began, but haven't progressed any further.";
+      case InitState.downloading:
+        return "We have begun downloading the certs.";
+      case InitState.downloaded:
+        return "We have downloaded all of the certs.";
+      case InitState.started:
+        return "A SmartLock bridge was created and started in initState()";
+    }
   }
 
   @override
@@ -158,8 +181,8 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.all(10),
             child: Column(
               children: [
-                const Text(
-                  'A SmartLock bridge was created and started in initState()',
+                Text(
+                  _getStateMessage(),
                   style: textStyle,
                   textAlign: TextAlign.center,
                 ),
